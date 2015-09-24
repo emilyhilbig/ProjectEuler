c9374ee92c87db110aa8f8b0441bbf16fb363e1e
Emily Hilbig <em.hilbig@gmail.com>
2015-09-24 17:26:49 -0400

    E4: Largest palindrome product

---
+++ euler4.py
@@ -0,0 +1,13 @@
+#length of number has to be 5-6 length between 10000 and 998001
+x = 67676767
+
+
+
+print str(100 * 100)
+print str(999 * 999)
+#length is at least 5, at most 6
+print str((988001 * 988001)/2)
+
+#if len(str(x)) == 3 :
+
+#import pdb; pdb.set_trace();
